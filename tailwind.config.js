4f4b603e0d5ed483f4fdb176137a533cff848665
SoufianeNajim <soufianenajim100@gmail.com>
2024-02-29 11:47:52 +0100

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    daisyui: {
+        themes: ["light", "dark", "cupcake"],
+    },
+    plugins: [require("daisyui")],
+};
